162ab7dd1dccfd1d1037939a5ce8a25f638036a6
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-12-10 16:12:53 -0300

    Metodos de sessão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Por favor, faça o login'
+      redirect_to login_url
+    end    
+  end
 end

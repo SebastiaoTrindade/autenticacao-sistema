7bea133305701a628665b87d6a93120b968db5ee
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-12-16 14:05:31 -0300

    Inserindo layout login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true  
+  validates :password, presence: true, length: {minimum:6, maximum:6}
   
 end

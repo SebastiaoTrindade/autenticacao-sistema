637d146a5a60da228089d91fd00aec51624b90f9
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-12-16 15:03:24 -0300

    Criando o controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # Session User
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   
+  # Session Login
   root 'sessions#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to:'sessions#destroy'
 
+  # Session Dashboard
   get 'dashboard', to: 'dashboard#index'
   
 end
